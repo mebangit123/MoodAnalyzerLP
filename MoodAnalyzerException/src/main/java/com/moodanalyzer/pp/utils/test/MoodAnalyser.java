430ee3de26fa11aadff66a59db8076c752f8a3f9
mebangit123 <mebannongrum333@gmail.com>
2021-04-27 15:26:12 +0530

    Handling moodanalyzer custom exception

--- MoodAnalyzerException/src/main/java/com/moodanalyzer/pp/utils/test/MoodAnalyser.java
+++ MoodAnalyzerException/src/main/java/com/moodanalyzer/pp/utils/test/MoodAnalyser.java
@@ -1,17 +1,32 @@
 package com.moodanalyzer.pp.utils.test;
 
+import com.moodanalyzer.pp.utils.test.MoodAnalyzerException.ExceptionType;
+
 public class MoodAnalyser {
 	String msg;
 	public MoodAnalyser(String msg) {
 		this.msg = msg;
 	}
-	public String analyseMood() {
+	public String analyseMood() throws MoodAnalyzerException {
 		if("Hello".equals(msg))
 			return "SAD";
 		else if("Happy".equals(msg))
 			return "HAPPY";
 		else if(msg == null)
-			return "HAPPY";
+			throw new MoodAnalyzerException(ExceptionType.NULL, "Null value was encountered");
+		else if(msg.length() == 0)
+			throw new MoodAnalyzerException(ExceptionType.EMPTY, "Empty string ");
 		return "";
+	}
+}
+
+class MoodAnalyzerException extends Exception {
+	public enum ExceptionType {
+		IVALID, EMPTY,NULL
+	}
+	ExceptionType type;
+	public MoodAnalyzerException(ExceptionType type, String msg) {
+		super(msg);
+		this.type = type;
 	}
 }
--- MoodAnalyzerException/src/test/java/com/moodanalyzer/pp/utils/test/MoodAnalyserTest.java
+++ MoodAnalyzerException/src/test/java/com/moodanalyzer/pp/utils/test/MoodAnalyserTest.java
@@ -4,23 +4,37 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.moodanalyzer.pp.utils.test.MoodAnalyzerException.ExceptionType;
+
 public class MoodAnalyserTest {
 	@Test
-	public void moodAnalyzerShouldReturnSad() {
+	public void moodAnalyzerShouldReturnSad() throws MoodAnalyzerException {
 		MoodAnalyser mood = new MoodAnalyser("Hello");
 		String moods = mood.analyseMood();
 		assertEquals("SAD",moods);
 	}
 	@Test
-	public void moodAnaylyzerShouldReturnHappy() {
+	public void moodAnaylyzerShouldReturnHappy() throws MoodAnalyzerException {
 		MoodAnalyser mood = new MoodAnalyser("Happy");
 		String moods = mood.analyseMood();
 		assertEquals("HAPPY",moods);
 	}
 	@Test
-	public void moodAnaylyzerWhenNullShouldReturnHappy() {
+	public void moodAnaylyzerWhenNullShouldThrowException() throws MoodAnalyzerException {
 		MoodAnalyser mood = new MoodAnalyser(null);
-		String moods = mood.analyseMood();
-		assertEquals("HAPPY",moods);
+		try {
+			mood.analyseMood();	
+		}catch(MoodAnalyzerException e) {
+			assertEquals(ExceptionType.NULL, e.type);
+		}
+	}
+	@Test
+	public void moodAnaylyzerWhenEmptyShouldReturnHappy() throws MoodAnalyzerException {
+		MoodAnalyser mood = new MoodAnalyser("");
+		try {
+			mood.analyseMood();	
+		}catch(MoodAnalyzerException e) {
+			assertEquals(ExceptionType.EMPTY, e.type);
+		}
 	}
 }
